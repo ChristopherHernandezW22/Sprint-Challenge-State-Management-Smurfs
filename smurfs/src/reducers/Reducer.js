9337ba95e368d137b3e55f74c026d1b8a63471a1
Christopher Hernandez <cerberean661@gmail.com>
2019-09-13 08:59:23 -0700

    setting up Reducer.js

---
+++ smurfs/src/reducers/Reducer.js
@@ -0,0 +1,10 @@
+import { FETCH_SMURF_START, FETCH_SMURF_SUCCESS, FETCH_SMURF_FAIL } from '../actions/Action';
+
+const initialState = {
+    smurfs: []
+};
+
+const reducer = (state = initialState, action) => {
+};
+
+export default reducer;
