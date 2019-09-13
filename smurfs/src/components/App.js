bd55565488b80a91883fd542ddb5d1ccf8a09bc3
Christopher Hernandez <cerberean661@gmail.com>
2019-09-13 11:39:23 -0700

    mvp

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,18 +1,27 @@
-import React, { Component } from "react";
+import React, { useEffect, Component } from "react";
 import "./App.css";
 import { connect } from 'react-redux';
 import { getSmurf } from '../actions/Action';
-import { addSmurf } from '../actions/Action';
 
+import Form from './Form';
 
 function App (props) {
   // console.log(props);
+  useEffect(() => {
+    props.getSmurf();
+  }, []);
+
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        {props.smurfs.map(smurf=> {
+          // console.log(smurf);
+        return<SmurfComponent smurf={smurf} key={smurf.id} />
+      })}
+      <Form /> 
       </div>
     );
 }
@@ -21,13 +30,18 @@ const SmurfComponent = (props) => {
   console.log(props);
   return (
   <div>
+    <p>{props.smurf.name},
+       {props.smurf.age}, 
+       {props.smurf.height}, 
+       {props.smurf.id}</p>
   </div>
   )
 }
 
-// const mapStateToProps = state => {
-//   return {
-//   };
-// };
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+  };
+};
 
-export default connect(null, {getSmurf})(App);
+export default connect(mapStateToProps, {getSmurf})(App);
--- smurfs/src/reducers/Reducer.js
+++ smurfs/src/reducers/Reducer.js
@@ -5,7 +5,6 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    
     switch(action.type) {
         case FETCH_SMURF_START:
             return {
